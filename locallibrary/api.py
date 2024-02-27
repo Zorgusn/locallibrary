90dbf9ed1dcccdc73f2b329bb248aa3adf386a97
Hudson Shaeffer <hudsonshaeffer@gmail.com>
2024-02-27 18:19:48 -0500

    added api to retrieve book by id and list of all books

--- locallibrary/api.py
+++ locallibrary/api.py
@@ -5,17 +5,6 @@ from django.shortcuts import get_object_or_404
 
 api = NinjaAPI()
 
-class HelloSchema(Schema):
-    name: str = "world"
-
-@api.post("/hello")
-def hello(request, data: HelloSchema):
-    return f"Hello {data.name}!"
-
-@api.get("/math/{a}and{b}")
-def math(request, a:int, b:int):
-    return {'add': a+b, "multiply":a*b}
-
 class BookOut(Schema):
     id: int
     title: str
